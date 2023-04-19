929f6f92ee19156b3c556cc2fc76718b8e688fa2
John Patrick Teruel <jpteruel95@gmail.com>
2023-04-19 18:55:24 +0800

    Initialized Xcode Project

---
+++ Anime Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anime Tracker
+//
+//  Created by John Patrick Teruel on 4/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
